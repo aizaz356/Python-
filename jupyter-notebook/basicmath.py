bbc606ac84fb922c328a8095d3d81242810c23c2
aizaz356 <aizaz3448@gmail.com>
2026-06-11 04:07:38 +0500

    add testing

---
+++ jupyter-notebook/basicmath.py
@@ -0,0 +1,21 @@
+def div(n1, n2):
+    return n1 / float(n2)
+
+
+
+#print (div(4, 2))
+
+
+
+
+
+
+def sortlist(lst):
+    alist = lst[:]            # copy the list
+    for passnum in range(len(alist)-1, 0, -1):
+        for i in range(passnum):
+            if alist[i] > alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+    return alist
